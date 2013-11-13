36cb8d3142b91bfe2d4300b5c53931dbc5d76e4d
Derek Hopper <hopper.derek@gmail.com>
2013-11-12 23:25:41 -0500

    Initial skeleton.

---
+++ lib/omniauth/assembla.rb
@@ -0,0 +1,7 @@
+require "omniauth/assembla/version"
+
+module Omniauth
+  module Assembla
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/assembla/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Assembla
+    VERSION = "0.0.1"
+  end
+end
